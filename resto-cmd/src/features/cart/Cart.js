f3644c7af7e2c919b79dd38889229a9ea68384d8
Yusef Boulam <yusef.boulam@gmail.com>
2024-09-05 21:28:47 +0200

    added 'ajouter super cremeux' button

--- resto-cmd/src/features/cart/Cart.js
+++ resto-cmd/src/features/cart/Cart.js
@@ -1,15 +1,24 @@
 import { useStore } from "react-redux";
+import { SuperCremeux } from "../../common/models";
+import { useEffect, useState } from "react";
 
 export const Cart = () => {
     const store = useStore();
+    const [list, setList] = useState(store.getState().list)
 
+    useEffect(() => {
+        store.subscribe(() => setList(store.getState().list))
+    })
 
     return <div className="Selection">
-        <h1>Liste de produits sélectionnés</h1>
+        <h1>Choisir son menu</h1>
+        <div className="CartNavBar">
+        </div>
         {
-            store.getState().list.map(
-                (item, index) => <span key={index} className="SelectedProduct">{item.title} {item.price} €</span>
+            list.map((item, index) => <span key={index} className="SelectedProduct">{item.title} {item.price} €</span>
             )
         }
+        <button onClick={() => store.dispatch({ type: 'ADD_PRODUCT', payload: SuperCremeux })}>Ajouter un super crémeux</button>
     </div>
+
 };
